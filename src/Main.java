c0fa00bb9d0d8cabf8ec3477a4b73f292c718cfd
Emmanu3l <22276642+Emmanu3l@users.noreply.github.com>
2022-12-19 11:39:15 +0100

    Solved day1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            day1();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void day1() throws IOException {
+        File file = new File("day1.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line;
+        ArrayList<Integer> elves = new ArrayList<>();
+        int currentSum = 0;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (!line.isBlank()) {
+                currentSum += Integer.parseInt(line);
+            } else {
+                elves.add(currentSum);
+                currentSum = 0;
+            }
+        }
+        System.out.println(Collections.max(elves));
     }
 }
